e7e06032947d04d106501d77018741f36db39eab
the_freestyle <the_freestyle@abv.bg>
2020-03-10 22:01:16 +0200

    Created repo

---
+++ ASP.NET Core/Data/IDFit.Data.Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace IDFit.Data.Models
+{
+    using IDFit.Data.Common.Models;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
