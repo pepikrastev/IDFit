dea71fc9df9ec08ae7df23b66c1d82bb8e855703
petar <p.h.krastev91@gmail.com>
2020-03-26 17:12:55 +0200

    Added functionality that gives role to users

---
+++ Web/IDFit.Web.ViewModels/Administration/Administration/UserRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace IDFit.Web.ViewModels.Administration.Administration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class UserRoleViewModel
+    {
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
--- Web/IDFit.Web/Areas/Administration/Controllers/AdministrationController.cs
+++ Web/IDFit.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -1,5 +1,6 @@
 ﻿namespace IDFit.Web.Areas.Administration.Controllers
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     using IDFit.Common;
@@ -107,8 +108,97 @@
                     return this.RedirectToAction("AllRoles");
                 }
 
+                foreach (var error in result.Errors)
+                {
+                    this.ModelState.AddModelError(" ", error.Description);
+                }
+
                 return this.View(model);
             }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUsersInRole(string roleId)
+        {
+            this.ViewBag.roleId = roleId;
+
+            var role = await this.roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                this.ViewBag.ErrorMessage = $"Role with id = {roleId} connot be found";
+                return this.View("NotFound");
+            }
+
+            var viewModel = new List<UserRoleViewModel>();
+
+            foreach (var user in this.userManager.Users)
+            {
+                var userRoleViewModel = new UserRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                };
+
+                if (await this.userManager.IsInRoleAsync(user, role.Name))
+                {
+                    userRoleViewModel.IsSelected = true;
+                }
+                else
+                {
+                    userRoleViewModel.IsSelected = false;
+                }
+
+                viewModel.Add(userRoleViewModel);
+            }
+
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> viewModel, string roleId)
+        {
+            var role = await this.roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                this.ViewBag.ErrorMessage = $"Role with id = {roleId} connot be found";
+                return this.View("NotFound");
+            }
+
+            for (int i = 0; i < viewModel.Count; i++)
+            {
+                var user = await this.userManager.FindByIdAsync(viewModel[i].UserId);
+
+                IdentityResult result = null;
+
+                if (viewModel[i].IsSelected && !(await this.userManager.IsInRoleAsync(user, role.Name)))
+                {
+                    result = await this.userManager.AddToRoleAsync(user, role.Name);
+                }
+                else if (!viewModel[i].IsSelected && await this.userManager.IsInRoleAsync(user, role.Name))
+                {
+                    result = await this.userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                else
+                {
+                    continue;
+                }
+
+                if (result.Succeeded)
+                {
+                    if (i < (viewModel.Count - 1))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return this.RedirectToAction("EditRole", new { Id = roleId });
+                    }
+                }
+            }
+
+            return this.RedirectToAction("EditRole", new { Id = roleId });
         }
     }
 }
